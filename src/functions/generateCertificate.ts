c908fea297bc02ba5f3136b7b07e89ebbec94a9e
pleaobraga <pleao.braga@gmail.com>
2021-07-04 10:04:04 -0300

    Create pdf and salve at Amazon

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,3 +1,4 @@
+import { S3 } from "aws-sdk";
 import chromium from "chrome-aws-lambda";
 import dayjs from "dayjs";
 import fs from "fs";
@@ -58,10 +59,43 @@ export const handle = async (event) => {
 
     const content = await compile(data);
 
+    const browser = await chromium.puppeteer.launch({
+      headless: true,
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+    });
+
+    const page = await browser.newPage();
+
+    await page.setContent(content);
+
+    const pdf = await page.pdf({
+      format: "a4",
+      landscape: true,
+      path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+      printBackground: true,
+      preferCSSPageSize: true,
+    });
+
+    await browser.close();
+
+    const s3 = new S3();
+    await s3
+      .putObject({
+        Bucket: "serverlesscertigficateignite",
+        Key: `${id}.pdf`,
+        ACL: "public-read",
+        Body: pdf,
+        ContentType: "application/pdf",
+      })
+      .promise();
+
     return {
       statusCode: 201,
       body: JSON.stringify({
         message: "Certificate Created",
+        url: `https://serverlesscertigficateignite.s3.us-west-1.amazonaws.com/${id}.pdf`,
       }),
       headers: {
         "Content-Type": "application/json",
